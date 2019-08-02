8a4d20b67ee1ab8502efd78608a014aeccf76353
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2019-08-02 13:59:11 +0700

    Update to vuetify 2.0 >

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import './plugins/vuetify';
+import vuetify from '@/plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,6 +7,7 @@ import store from './store';
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App),
